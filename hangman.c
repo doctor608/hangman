f5d61132b48f968aaaa9916a3d0f55a5eb8f7608
Artyom Makarov <InFo4U2004@mail.ru>
2020-06-04 19:06:45 +0300

    Make new functions: init_word_repr, draw_word_repr, get_user_guess, print_rules

--- hangman.c
+++ hangman.c
@@ -2,36 +2,71 @@
  * Hangman Game.
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define LIVES 6
+#define LIVES          6
+#define EMPTY_LETTER '_'
+#define PROMPT "GUESS: "
 
+void print_rules(void);
 const char* get_random_word(void);
 void draw_hangman(int lives);
+void draw_word_repr(const char* word_repr);
+char get_user_guess(const char* prompt);
+void init_word_repr(char* word_repr, int size, int val);
 
 int main(void)
 {
     int lives = LIVES;
     const char* word = get_random_word();
 
+    char word_repr[strlen(word) + 1];
+    init_word_repr(word_repr, sizeof(word_repr) - 1, EMPTY_LETTER);
+
+    print_rules();
+
+    int ch;
     for (;;) {
         draw_hangman(lives);
-        draw_word()
+        draw_word_repr(word_repr);
+        ch = get_user_guess(PROMPT);
+        printf("ch is %c\n", ch);
         getchar();
     }
 
-/*//      int len = strlen(argv[1]);*/
-
-/*//      int ch;*/
-/*//      ch = getchar(argv[1]);*/
-
-/*//      while (ch != EOF)*/
-/*//              if_symbols(ch, word[]);*/
-
     return 0;
+}
+
+char get_user_guess(const char* prompt)
+{
+    int ch;
+    while (printf("%s", prompt) && (ch = getchar()) != EOF) {
+        while (getchar() != '\n')
+            ;
+        if (isalpha(ch))
+            return ch;
+    }
+    return EOF;
+}
+
+void init_word_repr(char* word_repr, int size, int val)
+{
+    memset(word_repr, val, size);
+    word_repr[size] = '\0';
+}
+
+void print_rules(void)
+{
+    puts("Rules:");
+    puts("I will pick a random word.");
+    printf("%s %d %s\n", "You will have", LIVES,
+            "attempts to guess it, otherwise you will get hanged.");
+    puts("Press any key to begin...");
+    getchar();
 }
 
 /*
@@ -55,8 +90,17 @@ const char* get_random_word(void)
     return words[rand_pos];
 }
 
+void draw_word_repr(const char* word_repr)
+{
+    printf("\n%s", "WORD: ");
+    for (; *word_repr; ++word_repr)
+        printf("%c ", *word_repr);
+    puts("");
+}
+
 void draw_hangman(int lives)
 {
+    system("clear");
     switch (lives) {
     case 0:
         puts("|-----");
@@ -109,20 +153,3 @@ void draw_hangman(int lives)
         break;
     }
 }
-/*void check_symbol(char c, char word[])*/
-/*{*/
-    /*int len = strlen(word);*/
-    /*int num = 0;*/
-
-    /*for (int i = 0; i < len; ++i)*/
-    /*        if (c == word[i]) {*/
-    /*                for (int j = 0; j < i; ++j) {*/
-    /*                        putchar(' ');*/
-
-    /*                printf("%c", c);*/
-    /*                break;*/
-    /*                }*/
-    /*        }*/
-    /*        else {*/
-    /*        ++num;*/
-    /*        }*/
